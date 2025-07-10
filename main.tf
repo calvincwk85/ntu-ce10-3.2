a1a24fbfea835cdb41fb4ef8de602da45353bc22
calvincwk85 <calvincwk85@hotmail.com>
2025-07-10 20:43:13 +0800

    tf-update-2

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.9.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 2"
+      version = "~> 6.0"
     }
   }
 
